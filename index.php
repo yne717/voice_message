b803b282d1e7354165341d59b998ea22f0c82c55
ryosuke yamane <ryosuke@ryosuke-dsk01.(none)>
2013-11-05 00:24:10 +0900

    change

--- index.php
+++ index.php
@@ -2,34 +2,39 @@
 require ('lib/twilio/Services/Twilio.php');
 require ('config/config.php');
 
-$response = new Services_Twilio_Twiml();
 $config = new config();
 $id = $config->getTwilioId();
+$client = new Services_Twilio($id['sid'], $id['at']);
+$response = new Services_Twilio_Twiml();
 
 if (isset($_REQUEST['Digits'])) {
+	error_log($_REQUEST);
 	$input = $_REQUEST['Digits'];
 	switch ($input) {
+		//録音
 		case '1' :
 			$gather = $response -> gather(array('numDigits' => 1, 'timeout' => '10'));
 			$gather -> say('1 を押しやがりましたね、今月の請求額は、わかりません。', array('language' => 'ja-jp'));
 			break;
+			
+		//確認
 		case '2' :
 			$gather = $response -> gather(array('numDigits' => 1, 'timeout' => '10'));
 			$gather -> say('2 を押しやがりましたね、先月の請求額は、わかりません。', array('language' => 'ja-jp'));
 			break;
-		case '9' :
-			$response -> say('さようなら', array('language' => 'ja-jp'));
-			break;
+
 		default :
 			$gather = $response -> gather(array('numDigits' => 1, 'timeout' => '10'));
-			$gather -> say('1か2か9を押せって言ったじゃないの。やりなおし。', array('language' => 'ja-jp'));
+			$gather -> say('おおつるさん、やなぎもとさんへのメッセージを受付しております。
+            ボイスメッセージを登録する場合は１を、登録したボイスメッセージの確認は２を、もう一度再生する場合は３を押してください。', array('language' => 'ja-jp'));
 			break;
 	}
+	print $response;
+	
 } else {
-
-	$gather = $response -> gather(array('numDigits' => 1, 'timeout' => '10'));
+	$gather = $response -> gather(array('numDigits' => 1, 'timeout' => '20'));
 	$gather -> say("お電話有難うございます。おおつるさん、やなぎもとさんへのメッセージを受付しております。
-            ぴーっと音がなった後にメッセージをお願い致します。それではどうぞ", array('language' => 'ja-jp'));
+            ボイスメッセージを登録する場合は１を、登録したボイスメッセージの確認は２を、もう一度再生する場合は３を押してください。", array('language' => 'ja-jp'));
+	print $response;
+	
 }
-
-print $response;
