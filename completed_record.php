90d4fc81088412dca00adddf9ac51271a66cde98
ryosuke yamane <ryosuke@ryosuke-dsk01.(none)>
2013-11-09 02:57:11 +0900

    change

--- completed_record.php
+++ completed_record.php
@@ -3,7 +3,7 @@
 require ('logic/TwilioLogic.php');
 
 $logic = new TwilioLogic();
-error_log(print_r($logic->getParam()));
+// error_log(print_r($logic->getParam()));
 
 $response = $logic->completedRecord();
 echo $response;
