1ea90bc99cd8b373de78ca93694eac79269fa1e0
ryosuke yamane <ryosuke@ryosuke-dsk01.(none)>
2013-11-16 20:38:09 +0900

    change

--- logic/TwilioLogic.php
+++ logic/TwilioLogic.php
@@ -74,7 +74,7 @@ class TwilioLogic extends TwilioUtil{
 	//completed_record.php　録音完了後の確認コンタクト
 	public function completedRecordCheack() {
 		$response = $this->getTwiml();
-		$gather = $response->gather(array('numDigits' => 1, 'timeout' => '10'));
+		$gather = $response->gather(array('numDigits' => 1, 'timeout' => '10', 'action' => "/VM/completed_record_after.php"));
 		$gather->say("ろくおんがかんりょうしました。メッセージをろくおんしなおすばあいは1を、とうろくするばあいは9をおしてください", array('language' => 'ja-jp'));
 		$response->say("タイムアウトしました。もういちどさいしょからおねがいいたします。", array('language' => 'ja-jp'));
 		$response->hangup();
